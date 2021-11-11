558e0a2b027af8be7f70e20750a3a80fe35fef04
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2021-11-11 17:24:02 +0000

    Added GameObject class. Found no use for it yet

---
+++ AT_Project_01/GameObject.h
@@ -0,0 +1,17 @@
+#pragma once
+class Graphics;
+
+class GameObject
+{
+	public:
+		GameObject() = default;
+		virtual ~GameObject() = default;
+
+		GameObject(const GameObject&) = delete;
+		GameObject& operator=(const GameObject&) = delete;
+
+		virtual void Update(float dt) {}
+		virtual void Draw(Graphics* pGfx) {}
+
+	private:
+};
--- AT_Project_01/SceneTest.cpp
+++ AT_Project_01/SceneTest.cpp
@@ -31,7 +31,6 @@ void SceneTest::onCreate(SceneData& sceneData)
 	cameraManager.AddCamera(playerCamera);
 	cameraManager.AddCamera(staticCamera);
 
-
 	//std::string filePath[6];
 	//filePath[0].append("Assets\\Texture\\cubemap\\nx.png");
 	//filePath[1].append("Assets\\Texture\\cubemap\\ny.png");
@@ -52,102 +51,102 @@ void SceneTest::onCreate(SceneData& sceneData)
 	//cubeMap.LoadAndCreateTexture("Assets\\Texture\\StandardCubeMap.png");
 	//cubeMap.SetShaderResource(Bind::Texture::t2, 1u);
 
-	Material Red_plastic;
-	Red_plastic.ambient = { 0.0f, 0.0f, 0.0f, 1.0f };
-	Red_plastic.Diffuse = { 0.5f, 0.0f, 0.0f, 1.0f };
-	Red_plastic.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
-	Red_plastic.Specular = { 0.7f, 0.6f, 0.6f, 1.0f };
-	Red_plastic.SpecularPower = 32.0f;
+	//Material Red_plastic;
+	//Red_plastic.ambient = { 0.0f, 0.0f, 0.0f, 1.0f };
+	//Red_plastic.Diffuse = { 0.5f, 0.0f, 0.0f, 1.0f };
+	//Red_plastic.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
+	//Red_plastic.Specular = { 0.7f, 0.6f, 0.6f, 1.0f };
+	//Red_plastic.SpecularPower = 32.0f;
 
-	Material Emerald;
-	Emerald.ambient = { 0.0215f, 0.1745f, 0.0215f, 1.0f };
-	Emerald.Diffuse = { 0.07568f, 0.61424f, 0.07568f, 1.0f };
-	Emerald.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
-	Emerald.Specular = { 0.633f, 0.727811f, 0.633f, 1.0f };
-	Emerald.SpecularPower = 76.8f;
+	//Material Emerald;
+	//Emerald.ambient = { 0.0215f, 0.1745f, 0.0215f, 1.0f };
+	//Emerald.Diffuse = { 0.07568f, 0.61424f, 0.07568f, 1.0f };
+	//Emerald.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
+	//Emerald.Specular = { 0.633f, 0.727811f, 0.633f, 1.0f };
+	//Emerald.SpecularPower = 76.8f;
 
-	Material Gold;
-	Gold.ambient = { 0.24725f, 0.1995f, 0.0745f, 1.0f };
-	Gold.Diffuse = { 0.75164f, 0.60648f, 0.22648f, 1.0f };
-	Gold.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
-	Gold.Specular = { 0.628281f, 0.555802f, 0.366065f, 1.0f };
-	Gold.SpecularPower = 51.2f;
+	//Material Gold;
+	//Gold.ambient = { 0.24725f, 0.1995f, 0.0745f, 1.0f };
+	//Gold.Diffuse = { 0.75164f, 0.60648f, 0.22648f, 1.0f };
+	//Gold.Emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
+	//Gold.Specular = { 0.628281f, 0.555802f, 0.366065f, 1.0f };
+	//Gold.SpecularPower = 51.2f;
 
-	//spaceMarineHelmet.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\Helmet_paintable_v2.obj");
-	//spaceMarineHelmet.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_PBR.cso", ied, ied_size);
-	//spaceMarineHelmet.LoadTextures(sceneData.gfx, "Assets\\Texture\\Helmet_V3_Albedo.png");
-	//spaceMarineHelmet.LoadTextures(sceneData.gfx, "Assets\\Texture\\Helmet_V3_Normal.png");
-	//spaceMarineHelmet.transform.SetPosition(0.0f, 0.0f, 0.0f);
-	//spaceMarineHelmet.transform.SetRotation(0.0f, 3.0f, 0.0f);
+	spaceMarineHelmet.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\Helmet_paintable_v2.obj");
+	spaceMarineHelmet.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_PBR.cso", ied, ied_size);
+	spaceMarineHelmet.LoadTextures(sceneData.gfx, "Assets\\Texture\\Helmet_V3_Albedo.png");
+	spaceMarineHelmet.LoadTextures(sceneData.gfx, "Assets\\Texture\\Helmet_V3_Normal.png");
+	spaceMarineHelmet.transform.SetPosition(0.0f, 0.0f, 0.0f);
+	spaceMarineHelmet.transform.SetRotation(0.0f, 3.0f, 0.0f);
 
-	sphere.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\sphere.obj");
-	sphere.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
-	sphere.LoadTextures(sceneData.gfx, "Assets\\Texture\\icon.png");
-	sphere.transform.SetPosition(0.0f, 25.0f, 0.0f);
+	//sphere.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\sphere.obj");
+	//sphere.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
+	//sphere.LoadTextures(sceneData.gfx, "Assets\\Texture\\icon.png");
+	//sphere.transform.SetPosition(0.0f, 25.0f, 0.0f);
 
-	sky.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\inner_sphere.obj");
-	sky.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_unlit.cso", ied, ied_size);
-	sky.LoadTextures(sceneData.gfx, "Assets\\Texture\\syferfontein_0d_clear_1k.png");
-	sky.transform.SetScale(5000.0f, 5000.f, 5000.0f);
+	//sky.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\inner_sphere.obj");
+	//sky.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_unlit.cso", ied, ied_size);
+	//sky.LoadTextures(sceneData.gfx, "Assets\\Texture\\syferfontein_0d_clear_1k.png");
+	//sky.transform.SetScale(5000.0f, 5000.f, 5000.0f);
 
-	pyramid.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\pyramid.obj");
-	pyramid.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
-	pyramid.LoadTextures(sceneData.gfx, "Assets\\Texture\\default.png");
+	//pyramid.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\pyramid.obj");
+	//pyramid.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
+	//pyramid.LoadTextures(sceneData.gfx, "Assets\\Texture\\default.png");
 
-	single_cube.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\cube.obj");
-	single_cube.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
-	single_cube.LoadTextures(sceneData.gfx, "Assets\\Texture\\grass.png");
+	//single_cube.LoadMeshFromSource(sceneData.gfx, "Assets\\Model\\cube.obj");
+	//single_cube.LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
+	//single_cube.LoadTextures(sceneData.gfx, "Assets\\Texture\\grass.png");
 
 	// https://docs.microsoft.com/en-us/windows/uwp/gaming/complete-code-for-ddstextureloader
 	// https://www.3dgep.com/texturing-lighting-directx-11/#Light_Properties-2
 
-	float width = 10.0f;
-	float height = 10.0f;
+	//float width = 10.0f;
+	//float height = 10.0f;
 
-	float distance = 100.0f;
+	//float distance = 100.0f;
 
-	int amount = 1000;
+	//int amount = 1000;
 
-	float positionX = 0;
-	float positionY = 0;
-	float positionZ = 0;
+	//float positionX = 0;
+	//float positionY = 0;
+	//float positionZ = 0;
 
-	ModelLoader loader("Assets\\Model\\cube_proj.obj");
+	//ModelLoader loader("Assets\\Model\\cube_proj.obj");
 
-	for (int i = 0; i < amount; i++)
-	{
-		std::unique_ptr temp = std::make_unique<Model>();
-		temp->LoadMesh(sceneData.gfx, loader.GetVertices(), loader.GetIndices());
-		temp->LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
-		temp->LoadTextures(sceneData.gfx, "Assets\\Texture\\icon.png");
+	//for (int i = 0; i < amount; i++)
+	//{
+	//	std::unique_ptr temp = std::make_unique<Model>();
+	//	temp->LoadMesh(sceneData.gfx, loader.GetVertices(), loader.GetIndices());
+	//	temp->LoadShaders(sceneData.gfx, L"..\\x64\\Debug\\VS_Default.cso", L"..\\x64\\Debug\\PS_BlinnPhong.cso", ied, ied_size);
+	//	temp->LoadTextures(sceneData.gfx, "Assets\\Texture\\icon.png");
 
-		//if (i & 5)
-		//{
-		//	temp->SetMaterial(Emerald);
-		//}
-		//if (i & 10)
-		//{
-		//	temp->SetMaterial(Gold);
-		//}
+	//	//if (i & 5)
+	//	//{
+	//	//	temp->SetMaterial(Emerald);
+	//	//}
+	//	//if (i & 10)
+	//	//{
+	//	//	temp->SetMaterial(Gold);
+	//	//}
 
-		cube.push_back(std::move(temp));
-		cube.at(i)->transform.SetScale(5.0f, 5.0f, 5.0f);
-		cube.at(i)->transform.SetPosition(positionX - (distance * 10) / 2, positionY - (distance * 10) / 2, positionZ - (distance * 10) / 2);
+	//	cube.push_back(std::move(temp));
+	//	cube.at(i)->transform.SetScale(5.0f, 5.0f, 5.0f);
+	//	cube.at(i)->transform.SetPosition(positionX - (distance * 10) / 2, positionY - (distance * 10) / 2, positionZ - (distance * 10) / 2);
 
-		positionX += distance;
+	//	positionX += distance;
 
-		if (positionX >= (distance * width))
-		{
-			positionX = 0.0f;
-			positionY += distance;
+	//	if (positionX >= (distance * width))
+	//	{
+	//		positionX = 0.0f;
+	//		positionY += distance;
 
-			if (positionY >= (distance * height))
-			{
-				positionY = 0.0f;
-				positionZ += distance;
-			}
-		}
-	}
+	//		if (positionY >= (distance * height))
+	//		{
+	//			positionY = 0.0f;
+	//			positionZ += distance;
+	//		}
+	//	}
+	//}
 }
 
 void SceneTest::OnDestroy()
@@ -261,27 +260,27 @@ void SceneTest::Update(SceneData& sceneData)
 
 	sceneData.gfx->currentCamera.SetPosition(camera.GetPosition());
 
-	//spaceMarineHelmet.transform.SetRotation(0.0f, 0.05f * rot, 0.0f);
-	//spaceMarineHelmet.Update(sceneData.dt);
+	spaceMarineHelmet.transform.SetRotation(0.0f, 0.05f * rot, 0.0f);
+	spaceMarineHelmet.Update(sceneData.dt);
 
-	sphere.Update(sceneData.dt);
+	//sphere.Update(sceneData.dt);
 
-	//sky.transform.SetRotation(0.0f, 0.01f * rot, 0.0f);
-	sky.Update(sceneData.dt);
+	////sky.transform.SetRotation(0.0f, 0.01f * rot, 0.0f);
+	//sky.Update(sceneData.dt);
 
-	pyramid.transform.SetPosition(15.0f, 15.0f, 0.0f);
-	pyramid.transform.SetRotationAxis(0.5f * rot);
-	pyramid.Update(sceneData.dt);
+	//pyramid.transform.SetPosition(15.0f, 15.0f, 0.0f);
+	//pyramid.transform.SetRotationAxis(0.5f * rot);
+	//pyramid.Update(sceneData.dt);
 
-	single_cube.transform.SetRotation(0.0f, 0.0f, 0.5f * rot);
-	single_cube.transform.SetPosition(-15.0f, -15.0f, 0.0f);
-	single_cube.Update(sceneData.dt);
+	//single_cube.transform.SetRotation(0.0f, 0.0f, 0.5f * rot);
+	//single_cube.transform.SetPosition(-15.0f, -15.0f, 0.0f);
+	//single_cube.Update(sceneData.dt);
 
-	for (auto& cubes : cube)
-	{
-		cubes->transform.SetRotation(0.14f * rot , 0.2f * rot , 1.0f * rot);
-		cubes->Update(sceneData.dt);
-	}
+	//for (auto& cubes : cube)
+	//{
+	//	cubes->transform.SetRotation(0.14f * rot , 0.2f * rot , 1.0f * rot);
+	//	cubes->Update(sceneData.dt);
+	//}
 }
 
 void SceneTest::Draw(SceneData& sceneData)
@@ -300,14 +299,14 @@ void SceneTest::Draw(SceneData& sceneData)
 		sceneData.gfx->SetProjectionMatrix(camera2.GetProjectionMatrix());
 	}
 
-	//spaceMarineHelmet.Draw(sceneData.gfx);
-	sphere.Draw(sceneData.gfx);
-	sky.Draw(sceneData.gfx);
-	pyramid.Draw(sceneData.gfx);
-	single_cube.Draw(sceneData.gfx);
+	spaceMarineHelmet.Draw(sceneData.gfx);
+	//sphere.Draw(sceneData.gfx);
+	//sky.Draw(sceneData.gfx);
+	//pyramid.Draw(sceneData.gfx);
+	//single_cube.Draw(sceneData.gfx);
 
-	for (auto& cubes : cube)
-	{
-		cubes->Draw(sceneData.gfx);
-	}
+	//for (auto& cubes : cube)
+	//{
+	//	cubes->Draw(sceneData.gfx);
+	//}
 }
